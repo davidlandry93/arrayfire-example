c379e8339671e50d5bbc73b2adb7c5877d6ea936
David Landry <davidlandry93@gmail.com>
2017-02-13 14:23:52 -0500

    Added matrix matrix

--- src/examples.py
+++ src/examples.py
@@ -12,7 +12,7 @@ def time_fn(fn, *args, **kwargs):
     return (end_time - start_time, result)
 
 # Test de produit scalaire
-n = 100000
+n = 100000000
 print('==== Dot product of {}-vectors ===='.format(n))
 
 af.set_backend('cpu')
@@ -28,25 +28,8 @@ time_dot_product_device = af.timer.timeit(af.blas.matmul, v1, v2)
 print('Device time: {}'.format(time_dot_product_device))
 print('Speedup: {0:.2f}x'.format(time_dot_product_host / time_dot_product_device))
 
-# Test de sort
-size_of_vector = 1000000
-print('==== Sort an array of {} floats ===='.format(size_of_vector))
-
-af.set_backend('cpu')
-v = af.random.randu(size_of_vector)
-time_sort_host = af.timer.timeit(af.algorithm.sort, v)
-print('Host time: {}'.format(time_sort_host))
-
-af.set_backend('cuda')
-v = af.random.randu(size_of_vector)
-time_sort_device = af.timer.timeit(af.algorithm.sort, v)
-print('Device time: {}'.format(time_sort_device))
-print('Speedup: {0:.2f}x'.format(time_sort_host / time_sort_device))
-
-
-
-# Test opérations matricielles
-n = 15000
+# Matrice-Vecteur
+n = 10000
 
 print('')
 print('==== Multiply a matrix of {}x{} with a {}-vector ===='.format(n, n, n))
@@ -65,7 +48,28 @@ print('Device time: {}'.format(time_matmul_device))
 print('Speedup: {0:.2f}x'.format(time_matmul_host / time_matmul_device))
 
 
-n = 100
+# Matrice-Matrice
+n = 1000
+
+print('')
+print('==== Multiply matrices of {}x{} ===='.format(n, n))
+
+af.set_backend('cpu')
+m = af.random.randu(n, n)
+a = af.random.randu(n, n)
+time_matmul2_host = af.timer.timeit(af.blas.matmul, m, a)
+print('Host time: {}'.format(time_matmul2_host))
+
+af.set_backend('cuda')
+m = af.random.randu(n, n)
+a = af.random.randu(n, 1)
+time_matmul2_device = af.timer.timeit(af.blas.matmul, m, a)
+print('Device time: {}'.format(time_matmul_device))
+print('Speedup: {0:.2f}x'.format(time_matmul2_host / time_matmul2_device))
+
+
+# Solve linear equation
+n = 500
 print('')
 print('==== Solve a {}x{} linear equation ===='.format(n, n, n))
 
@@ -86,8 +90,20 @@ b = af.blas.matmul(m, a)
 time_les_device = af.timer.timeit(af.solve, m, b)
 print('Device time: {}'.format(time_les_device))
 print('Speedup: {0:.2f}x'.format(time_les_host / time_les_device))
+# Test de sort
+size_of_vector = 1000000
+print('==== Sort an array of {} floats ===='.format(size_of_vector))
 
+af.set_backend('cpu')
+v = af.random.randu(size_of_vector)
+time_sort_host = af.timer.timeit(af.algorithm.sort, v)
+print('Host time: {}'.format(time_sort_host))
 
+af.set_backend('cuda')
+v = af.random.randu(size_of_vector)
+time_sort_device = af.timer.timeit(af.algorithm.sort, v)
+print('Device time: {}'.format(time_sort_device))
+print('Speedup: {0:.2f}x'.format(time_sort_host / time_sort_device))
 
 n = 1000
 print('')
